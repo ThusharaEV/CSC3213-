666658a6cec4efc3c52fe19d4f6e01fb8c5b88d6
ThusharaEV <131328112+ThusharaEV@users.noreply.github.com>
2026-03-28 21:38:02 +0530

    Arithmatic operations

---
+++ code1-2.asm
@@ -0,0 +1,18 @@
+.model small
+.stack 100h
+.data
+n1 db 2
+.code
+start:
+mov ax,@data
+mov ds,ax
+
+mov dl,n1;dl=2
+add dl,48  ;dl=dl+48=2+48=50
+mov ah,02h
+int 21h 
+
+
+mov ax,4c00h
+int 21h
+end start
